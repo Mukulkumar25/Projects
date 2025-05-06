fd1e755e5c6fa463d658c675f157f067a98c8bfe
Mukulkumar25 <mukul25164707@gmail.com>
2025-05-06 21:18:12 +0530

    minor updates

--- GreenCart/client/src/context/AppContext.jsx
+++ GreenCart/client/src/context/AppContext.jsx
@@ -5,7 +5,7 @@ import toast from "react-hot-toast";
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 
 export const AppContext = createContext();
 
