a7468cf76f0d74fc1fced363d1b837bd0d9ce2c8
Mukulkumar25 <mukul25164707@gmail.com>
2025-05-07 12:41:15 +0530

    minor updates

--- GreenCart/client/src/context/AppContext.jsx
+++ GreenCart/client/src/context/AppContext.jsx
@@ -5,7 +5,7 @@ import toast from "react-hot-toast";
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL || import.meta.env.VITE_API_BASE_URL;
 
 export const AppContext = createContext();
 
--- GreenCart/client/src/pages/seller/ProductList.jsx
+++ GreenCart/client/src/pages/seller/ProductList.jsx
@@ -50,6 +50,7 @@ const ProductList = () => {
                                             <span className="dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5"></span>
                                         </label>
                                     </td>
+
                                 </tr>
                             ))}
                         </tbody>
